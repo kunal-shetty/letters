9e604fac822cb06865de9c0dd073ea944ce42cda
kunal-shetty <kunalshetty567@gmail.com>
2025-06-05 22:41:19 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'Message Board',
+  title:  '💌 unsent letters',
   description: 'A simple message board app',
 }
 
